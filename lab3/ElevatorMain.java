b5d93b75c04b0c75d26fcdb5788a9bc4c0112055
yomikaze <ypeter999@gmail.com>
2014-10-19 16:58:34 -0400

    added TestEventBarrier and ElevatorMain

---
+++ lab3/ElevatorMain.java
@@ -0,0 +1,23 @@
+
+public class ElevatorMain {
+	public static void main(String args[]) {
+		if(args.length == 0) {
+			// No options specified; make the default as the part 3 elevator submission
+		} else if(args.length > 2) {
+			System.out.println("Too many arguments");
+		} else {
+			if(args[1].equals("p1")) {
+				System.out.println("Testing EventBarrier");
+				TestEventBarrier.test();
+			}
+			if(args[1].equals("p2part1")) {
+				// call the elevator part1
+			} else if(args[1].equals("p2part2")) {
+				// call the elevator part2
+			} else if(args[1].equals("p2part3")) {
+				// call the elevator part3
+			}
+			// ...
+		}
+	}
+}
---
+++ lab3/TestEventBarrier.java
@@ -0,0 +1,8 @@
+
+
+public class TestEventBarrier {
+	public static void test() {
+		EventBarrier eb1 = new EventBarrier();
+		EventBarrier eb2 = new EventBarrier();
+	}
+}
