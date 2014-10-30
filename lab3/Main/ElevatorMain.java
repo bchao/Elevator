61620e921db5a8f24df13be8e8c8f65a6a82a329
yomikaze <ypeter999@gmail.com>
2014-10-30 19:18:55 -0400

    minor edit

--- lab3/Main/ElevatorMain.java
+++ lab3/Main/ElevatorMain.java
@@ -16,18 +16,22 @@ public class ElevatorMain {
 		} else if(args.length > 2) {
 			System.out.println("Too many arguments");
 		} else {
-			if(args[1].equals("p1")) {
+			if(args[0].equals("p1")) {
 				System.out.println("Testing EventBarrier");
 				//TestEventBarrier.test();
 			}
-			if(args[1].equals("p2part1")) {
-				// call the elevator part1
-				
-				
-			} else if(args[1].equals("p2part2")) {
-				// call the elevator part2
-			} else if(args[1].equals("p2part3")) {
-				// call the elevator part3
+			if(args[0].equals("p2part1")) {
+				System.out.println("Problem 2 Part 1");
+				PartOneInitializer p2p1 = new PartOneInitializer();
+				p2p1.beginSimulation();
+			} else if(args[0].equals("p2part2")) {
+				System.out.println("Problem 2 Part 2");
+				PartTwoInitializer p2p2 = new PartTwoInitializer();
+				p2p2.beginSimulation();
+			} else if(args[0].equals("p2part3")) {
+				System.out.println("Problem 2 Part 3");
+				PartThreeInitializer p2p3 = new PartThreeInitializer();
+				p2p3.beginSimulation();
 			}
 			// ...a
 		}
